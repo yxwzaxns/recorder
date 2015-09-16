f7cf471e0f9c084529191ec96281784e32bd2783
aong <578011150@qq.com>
2015-09-16 18:56:37 +0800

    Update and rename home.php to Home.php

--- src/application/controllers/Home.php
+++ src/application/controllers/Home.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class home extends CI_Controller {
+class Home extends CI_Controller {
 	public function __construct()
     {
         parent::__construct();
